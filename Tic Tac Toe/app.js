5b93efc182750de55c07efe355eccdd25dfeb338
Jawaid Aziz <135430447+javaid24@users.noreply.github.com>
2024-03-31 03:20:35 +0500

    commit

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -3,12 +3,12 @@ let resetBtn = document.querySelector("#reset-btn");
 
 let turn_O = true;
 
-const winPattterns = [
+const winPatterns = [
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
     [1, 4, 7],
-    [5, 5, 8],
+    [2, 5, 8],
     [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8],
@@ -17,6 +17,7 @@ const winPattterns = [
 const resetGame = () => {
     turn_O = true;
     enableBoxes();
+    removeWinningClass(); // Reset the winning class
 };
 
 const disableBoxes = () => {
@@ -31,6 +32,26 @@ const enableBoxes = () => {
     {
         box.disabled = false;
         box.innerText = "";
+        box.classList.remove("winning-box"); // Remove winning class
+    }
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns)
+    {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val)
+        {
+            // Highlight the winning boxes
+            boxes[pattern[0]].classList.add("winning-box");
+            boxes[pattern[1]].classList.add("winning-box");
+            boxes[pattern[2]].classList.add("winning-box");
+
+            disableBoxes();
+        }
     }
 };
 
@@ -48,21 +69,7 @@ boxes.forEach ( (box) => {
         }
         box.disabled = true;
 
-        chkWinner = () => {
-            for(let pattern of winPattterns)
-            {
-                let pos1Val = boxes[pattern[0]].innerText;
-                let pos2Val = boxes[pattern[1]].innerText;
-                let pos3Val = boxes[pattern[2]].innerText;
-            }
-            if(pos1Val != "" && pos2Val != "" && pos3Val != "")
-            {
-                if(pos1Val === pos2Val && pos2Val === pos3Val)
-                {
-                    disableBoxes();
-                }
-            }
-        }
+        checkWinner();
     });
 });
 
