bb133e70b6709bd084bab26d9bead15dc13519fd
Jawaid Aziz <135430447+javaid24@users.noreply.github.com>
2024-06-12 13:35:49 +0500

    commit

--- React/newfolder/your_app_name/src/index.js
+++ React/newfolder/your_app_name/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const MyCompo = () => {
+  const name = "Javaid Memon";
+  const x = '6' + '9';
+  return <h1>Hello {name}, {x}</h1>
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<MyCompo></MyCompo>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
