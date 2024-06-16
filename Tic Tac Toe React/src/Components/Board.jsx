bf0924521ab8edb9ed8fbb78a441b348dda00c5f
Jawaid Aziz <135430447+javaid24@users.noreply.github.com>
2024-06-16 20:06:53 +0500

    commit

--- Tic Tac Toe React/src/Components/Board.jsx
+++ Tic Tac Toe React/src/Components/Board.jsx
@@ -6,19 +6,47 @@ const Board = () => {
     const [isXTurn,setIsXTurn] = useState(true);
     
     const checkWinner = () => {
-        
-    }
+        const winner = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+        for(let logic of winner) {
+            const [a,b,c] = logic;
+            if(state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+                return true;
+            };
+        };
+        return false;
+    };
+
+    const isWinner = checkWinner();
 
     const handleClick = (index) => {
+        if(state[index] !== null) {
+            return;
+        }
         const copyState = [...state];
         copyState[index] = isXTurn ? "X" : "O";
         setState(copyState);
         setIsXTurn(!isXTurn);
     };
 
+    const handleReset = () => {
+        setState(Array(9).fill(null));
+    };
 
     return (
         <div className="board-container">
+            {isWinner ? (<>{isWinner} won the game.{" "}
+            <button onClick={handleReset}>Play Again</button></>) :<></> }
+            <h4>Player {isXTurn ? "X" : "O"} please move!</h4>
             <div className="board-row">
                 <Square onClick={ () => handleClick(0)} value={state[0]}/>
                 <Square onClick={ () => handleClick(1)} value={state[1]}/>
